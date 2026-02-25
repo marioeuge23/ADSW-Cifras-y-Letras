bbb05735a12c4370f0abd732f3e5babb5874f06f
mario <mario@10.241.41.81>
2026-02-25 17:14:21 +0100

    Prueba para ver si esto funciona o no

---
+++ ADSW26_CifrasyLetras/src/main/java/es/upm/dit/adsw/cifrasyletras/juego/PruebaSincro.java
@@ -0,0 +1,11 @@
+package es.upm.dit.adsw.cifrasyletras.juego;
+
+public class PruebaSincro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("DING DONG, son las 17:15");
+
+	}
+
+}
